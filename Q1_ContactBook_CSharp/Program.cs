1fc34ce258789731079bc2625a25b79c4e074a50
20068451 <20068451@mydbs.ie>
2025-12-08 08:45:43 +0000

    Add Sample Contacts and Add Function ( 1 & 2 )
    Add Sample Contacts and Add Function ( 1 & 2 )

--- Q1_ContactBook_CSharp/Program.cs
+++ Q1_ContactBook_CSharp/Program.cs
@@ -75,11 +75,93 @@ namespace ContactBookApp
         }
     }
 
+    public class ContactBook
+    {
+        private List<Contact> contacts = new List<Contact>();
+        public ContactBook()
+        {
+            SampleContacts();
+        }
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+        public void AddContact(string firstName, string lastName, string company,
+                               string mobileNumber, string email, DateTime birthDate)
+        {
+            Contact c = new Contact(firstName, lastName, company, mobileNumber, email, birthDate);
+            contacts.Add(c);
+        }
+        public void ShowAllContacts()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("No contacts");
+                return;
+            }
+
+            Console.WriteLine("----- All Contacts -----");
+            for (int i = 0; i < contacts.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ": " + contacts[i].GetSummary());
+            }
+        }
+                public void ShowContactDetails(int index)
+        {
+            if (index < 0 || index >= contacts.Count)
+            {
+                Console.WriteLine("Invalid contact number.");
+                return;
+            }
+
+            Console.WriteLine("----- Contact Details -----");
+            contacts[index].ShowDetails();
+        }
+                public static bool IsValidMobile(string mobile)
+        {
+            if (mobile.Length != 9)
+                return false;
+
+            foreach (char ch in mobile)
+            {
+                if (!char.IsDigit(ch))
+                    return false;
+            }
+
+            if (mobile == "000000000")
+                return false;
+
+            return true;
+        }
+        private void SampleContacts()
+        {
+            AddContact("Wai", "Aung", "Dublin Business School", "799899361", "waiaung@dbs.ie", new DateTime(1990, 1, 1));
+            AddContact("Thukha", "Aung", "ABC", "799899362", "thukhaaung@abc.com", new DateTime(1985, 5, 10));
+            AddContact("Tet Tun", "Kyaw", "XYZ Ltd", "799899363", "tettunkyaw@xyz.com", new DateTime(1992, 3, 20));
+            AddContact("Zaw Htet", "Aung", "Tech Co", "799899364", "zawhtetaung@tech.com", new DateTime(1988, 7, 15));
+            AddContact("Pai", "Pai", "Finance Inc", "799899365", "paipai@fin.com", new DateTime(1991, 9, 30));
+            AddContact("Kyaw Thet", "Paing", "Retail Corp", "799899366", "kyawthetpaing@retail.com", new DateTime(1983, 11, 5));
+            AddContact("Wai", "Yan", "HealthCare", "799899367", "waiyan@health.com", new DateTime(1994, 4, 18));
+            AddContact("Nyi", "Nyi", "Travel Co", "799899368", "nyinyi@travel.com", new DateTime(1987, 8, 8));
+            AddContact("Aung", "Aung", "Food Co", "799899369", "aungaung@food.com", new DateTime(1993, 2, 2));
+            AddContact("Wai", "Mon", "Media Ltd", "799899370", "waimon@media.com", new DateTime(1989, 6, 6));
+            AddContact("Aung Myo", "Min", "Bank", "799899371", "aungmyomin@bank.com", new DateTime(1990, 12, 12));
+            AddContact("Nyi Nyi", "Maung", "DBS", "799899372", "nyinyimaung@dbs.ie", new DateTime(1986, 1, 25));
+            AddContact("Ko", "Maung", "School", "799899373", "komaung@school.com", new DateTime(1995, 10, 10));
+            AddContact("Aung", "Nyi", "Shop", "799899374", "aungnyi@shop.com", new DateTime(1984, 9, 9));
+            AddContact("Tin Mg Mg", "Tun", "Gym", "799899375", "tinmgmgtun@gym.com", new DateTime(1992, 11, 11));
+            AddContact("Nyi", "Bwar", "Restaurant", "799899376", "nyibwar@rest.com", new DateTime(1987, 3, 3));
+            AddContact("Ko", "Ko", "Hotel", "799899377", "koko@hotel.com", new DateTime(1991, 4, 4));
+            AddContact("Sein", "Khanig", "Logistics", "799899378", "seinkhanig@logi.com", new DateTime(1985, 5, 5));
+            AddContact("Thar", "Thar", "Clinic", "799899379", "tharthar@clinic.com", new DateTime(1993, 6, 6));
+            AddContact("Chan", "Chan", "DBS", "799899380", "chanchan@dbs.ie", new DateTime(1988, 7, 7));
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Contact Book App");
+            ContactBook contactBook = new ContactBook();
             bool exit = false;
 
             while (!exit)
@@ -91,13 +173,13 @@ namespace ContactBookApp
                 switch (choice)
                 {
                     case "1":
-                        Console.WriteLine("Add Contact");
+                        AddContactUI(contactBook);
                         break;
                     case "2":
-                        Console.WriteLine("Show All Contacts");
+                        contactBook.ShowAllContacts();
                         break;
                     case "3":
-                        Console.WriteLine("Show Contact Details");
+                        Console.WriteLine("Contact Details");
                         break;
                     case "4":
                         Console.WriteLine("Update Contact");
@@ -130,6 +212,46 @@ namespace ContactBookApp
             Console.WriteLine("5: Delete Contact");
             Console.WriteLine("0: Exit");
             Console.WriteLine("-----------------------------------------");
+        }
+
+        static void AddContactUI(ContactBook contactBook)
+        {
+            Console.WriteLine("----- Add Contact -----");
+
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Company: ");
+            string company = Console.ReadLine();
+
+            string mobile;
+            while (true)
+            {
+                Console.Write("Mobile Number (9 digits): ");
+                mobile = Console.ReadLine();
+                if (ContactBook.IsValidMobile(mobile))
+                    break;
+                Console.WriteLine("Invalid Mobile Number");
+            }
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            DateTime birthDate;
+            while (true)
+            {
+                Console.Write("Birthdate (e.g. 12/31/1990): ");
+                string inputDate = Console.ReadLine();
+                if (DateTime.TryParse(inputDate, out birthDate))
+                    break;
+                Console.WriteLine("Invalid date. Try again.");
+            }
+
+            contactBook.AddContact(firstName, lastName, company, mobile, email, birthDate);
+            Console.WriteLine("Contact Added.");
         }
     }
 }
